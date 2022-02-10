53394eac67e094c9c2644915fa63f900e1ba483e
sahas <99327155+IT21312380@users.noreply.github.com>
2022-02-10 14:30:34 +0000

    what did you change

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,23 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+
+  float distance;
+  float amount;
+
+  printf("enter total distence :");
+  scanf("%f",&distance);
+
+  if(distance<=30)
+  {
+      amount=distance*50;
+      printf("amount is : %.2f",amount);
+  }
+  else
+  {
+    amount= 30*50 +(distance-30)*40;
+    printf("amount is : %.2f",amount); 
+  }
   
   return 0;
 }
